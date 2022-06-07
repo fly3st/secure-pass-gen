0daab2950486e5128fae385dbab828952779395d
Daniel Huri <danielhuri.invtb@gmail.com>
2022-06-07 22:29:15 +0300

    Added password generation

--- app.py
+++ app.py
@@ -1,13 +1,13 @@
-from re import T
-
+from cgi import print_form
+from time import sleep
+import pyperclip
+import random
+import string
 
 MAX_PASS_LENGTH = 128
 MIN_PASS_LENGTH = 20
 
-symbols = False
-numbers = False
-upper = False
-lower = False
+final_pass = ''
 
 run = True
 
@@ -62,58 +62,145 @@ while run:
 
     prompt = True
 
-    # Include numbers
-    print('Include numbers? [Y/N]')
-
-    user_input = input('> ') 
     while prompt:
-        if user_input == 'Y' or user_input == 'y':
-            numbers = True
-            break
-        elif user_input == 'N' or user_input == 'n':
-            break
-        else:
-            print('These are not valid characters.')
-            continue
+        symbols = False
+        numbers = False
+        upper = False
+        lower = False
 
-    #Include uppercase
-    print('Include uppercase letters? [Y/N]')
+        # Include numbers
+        print('Include numbers? [Y/N]')
 
-    user_input = input('> ')
-    while prompt:
-        if user_input == 'Y' or user_input == 'y':
-            upper = True
-            break
-        elif user_input == 'N' or user_input == 'n':
-            break
-        else:
-            print('These are not valid characters.')
-            continue
+        while prompt:
+            user_input = input('> ')
+            if user_input == 'Y' or user_input == 'y':
+                numbers = True
+                break
+            elif user_input == 'N' or user_input == 'n':
+                break
+            else:
+                print('These are not valid characters.')
+                continue
+
+        #Include uppercase
+        print('Include uppercase letters? [Y/N]')
+
+        while prompt:
+            user_input = input('> ')
+            if user_input == 'Y' or user_input == 'y':
+                upper = True
+                break
+            elif user_input == 'N' or user_input == 'n':
+                break
+            else:
+                print('These are not valid characters.')
+                continue
+            
+        #Include lowercase
+        print('Include lowercase letters? [Y/N]')
+
+        while prompt:
+            user_input = input('> ')
+            if user_input == 'Y' or user_input == 'y':
+                lower = True
+                break
+            elif user_input == 'N' or user_input == 'n':
+                break
+            else:
+                print('These are not valid characters.')
+                break
+
+        #Include symbols
+        print('Include symbols? [Y/N]')
+
+        while prompt:
+            user_input = input('> ')
+            if user_input == 'Y' or user_input == 'y':
+                symbols = True
+                break
+            elif user_input == 'N' or user_input == 'n':
+                break
+            else:
+                print('These are not valid characters.')
+                continue
         
-     #Include lowercase
-    print('Include lowercase letters? [Y/N]')
-
-    user_input = input('> ')
-    while prompt:
-        if user_input == 'Y' or user_input == 'y':
-            lower = True
-            break
-        elif user_input == 'N' or user_input == 'n':
-            break
-        else:
-            print('These are not valid characters.')
-            break
-
-    #Include symbols
-    print('Include symbols? [Y/N]')
-
-    user_input = input('> ')
-    while prompt:
-        if user_input == 'Y' or user_input == 'y':
-            symbols = True
-            break
-        elif user_input == 'N' or user_input == 'n':
-            break
-        else:
-            print('These are not valid characters.')
+        if numbers == False and symbols == False:
+            print('A password must contain numbers and symbols.')
             continue
+        elif symbols == False and upper == False and lower == False:
+            print('A password must contain symbols and letters.')
+            continue
+        elif upper == False and lower == False:
+            print('A password must contain letters.')
+            continue
+        else:
+            prompt = False
+            break
+
+    print('Generating password...')
+    sleep(3)
+
+    #Generate password function
+    def gen_pass(number, upper, lower, symbol, user_pass):
+        if number == True and upper == True and lower == True and symbol == True:
+            user_pass = ''.join(random.choice(string.punctuation + string.digits + string.ascii_letters) for i in range(int(pass_length)))            
+            print(user_pass)
+        elif number == True and upper == True and symbol == True:
+            user_pass = ''.join(random.choice(string.punctuation + string.digits + string.ascii_uppercase) for i in range(int(pass_length)))
+            print(user_pass)
+        elif number == True and lower == True and symbol == True:
+            user_password = ''.join(random.choice(string.punctuation + string.digits + string.ascii_lowercase) for i in range(int(pass_length)))
+            print(user_pass)
+
+    gen_pass(numbers, upper, lower, symbols, final_pass)
+
+    # Prompt user to copy to clipboard
+    print('Congratulations on your new password! Would you like to copy to clipboard? [Y/N]')
+
+    user_input = input('> ')
+
+    prompt = True
+    
+    while prompt:
+        if user_input == 'Y' or user_input == 'y':
+            pyperclip.copy(final_pass)
+            print('Password has been successfully copied!')
+            break
+        elif user_input == 'N' or user_input == 'n':
+            break
+        else:
+            print('This is not a valid answer.')
+            continue
+    
+    
+    # Prompt user to export to file
+    print('Would you like to export the password to a text file? [Y/N]')
+
+    user_input = input('> ')
+
+    prompt = True
+    
+    while prompt:
+        if user_input == 'Y' or user_input == 'y':
+            with open('Password.txt', 'w') as p:
+                p.write(final_pass)
+            print('Password successfully exported to file!')
+            break
+        elif user_input == 'N' or user_input == 'n':
+            break
+        else:
+            print('This is not a valid answer.')
+            continue
+            
+    # Prompt user to restart
+    print('Would you like to generate a new password again? [Y/N]')
+
+    user_input = input('> ')
+
+    if user_input == 'Y' or user_input == 'y':
+        continue
+    else:
+        print('Goodbye!')
+        sleep(3)
+        run = False
+        break
